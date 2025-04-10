331adbbf9bfab5f5b005d712465d05d0ad13568c
Kenny Carter <kennycocopuff@gmail.com>
2025-04-10 14:01:27 -0700

    update to config

---
+++ scripts/docker.sh
@@ -0,0 +1,10 @@
+sudo dnf -y install dnf-plugins-core 
+
+sudo dnf-3 config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+
+sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo systemctl enable --now docker
+
+sudo docker run hello-world
+
