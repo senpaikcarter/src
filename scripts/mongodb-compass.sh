d765c4860f8003564f117ffb6d5bd3ac100e9c6f
Kenny Carter <kennycocopuff@gmail.com>
2026-01-19 09:35:52 -0800

    mongodb-compass

---
+++ scripts/mongodb-compass.sh
@@ -0,0 +1,6 @@
+echo "Downloading MongoDB Compass..."
+wget https://downloads.mongodb.com/compass/mongodb-compass-1.46.10.x86_64.rpm
+echo "Download complete."
+echo "Installing MongoDB Compass..."
+sudo yum install mongodb-compass-1.46.10.x86_64.rpm
+echo "Installation complete."
